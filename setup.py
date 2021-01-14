63ca5d70206e09550c4c8525f44aff0de4489b17
Roger Vilà <rogervila@me.com>
2021-01-14 17:27:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='pymongo_cursor_json_encoder',
-    packages=['pymongo_cursor_json_encoder'],
-    version='0.2.0',
+    name='PymongoCursorJSONEncoder',
+    packages=['PymongoCursorJSONEncoder'],
+    version='0.3.0',
     license='MIT',
     description='Convert pymongo Cursor objects to JSON',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Roger Vilà',
     author_email='rogervila@me.com',
-    url='https://github.com/rogervila/pymongo-cursor-json-encoder',
-    download_url='https://github.com/rogervila/pymongo-cursor-json-encoder/archive/0.2.0.tar.gz',
+    url='https://github.com/rogervila/PymongoCursorJSONEncoder',
+    download_url='https://github.com/rogervila/PymongoCursorJSONEncoder/archive/0.3.0.tar.gz',
     keywords=['pymongo', 'cursor', 'json', 'encoder'],
     install_requires=[
         'isodate',
