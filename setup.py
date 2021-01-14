dd2411f5a714dc8850d1b017c90e2e7233dfb0e1
Roger Vilà <rogervila@me.com>
2021-01-14 17:16:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pymongo-cursor-json-encoder',
     packages=['pymongo-cursor-json-encoder'],
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='Convert pymongo Cursor objects to JSON',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Roger Vilà',
     author_email='rogervila@me.com',
     url='https://github.com/rogervila/pymongo-cursor-json-encoder',
-    download_url='https://github.com/rogervila/pymongo-cursor-json-encoder/archive/0.1.0-rc.1.tar.gz',
+    download_url='https://github.com/rogervila/pymongo-cursor-json-encoder/archive/0.2.0.tar.gz',
     keywords=['pymongo', 'cursor', 'json', 'encoder'],
     install_requires=[
         'isodate',
